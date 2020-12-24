9f75797b58f91e79ddde1cd508c9a776d49c2e5b
js-jun <2432041161@qq.com>
2020-12-24 17:37:55 +0800

    提交创建

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+//引入axios
+import axios from 'axios'
+import qs from 'qs'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  created() {
+    let qq = qs.stringify({ num: 123 })
+    axios.post('/postdata', {
+      data: qq
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   }
 }
 </script>
